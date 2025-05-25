63ced81ea7590f796e35d5b5c10f05e76a477af5
baadillahnabil <baadillahnabil@gmail.com>
2025-05-25 15:41:28 +0700

    feat: add RightSidebar component and TagCard for improved navigation and display of popular tags

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { techMap } from "@/constants/techMap"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getDevIconClassName = (name: string) => {
+  const normalized = name.replace(/[ .]/g, "").toLowerCase()
+
+  return techMap[normalized]
+    ? `${techMap[normalized]} colored`
+    : `devicon-devicon-plain`
 }
